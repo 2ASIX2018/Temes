fcb1dea0a7d82892e8e3bffcfc17d5bb5d34e204
eljust <joamuran@gmail.com>
2019-01-29 23:53:27 +0100

    Fixats errors a la capçalera i s'ha afegit php per a post estàndard

--- elMeuTema/header.php
+++ elMeuTema/header.php
@@ -24,51 +24,7 @@
       <div class="container">
         
    <a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Portada
-                <!--span class="sr-only">(current)</span-->
-              </a>
-            </li>
-
-            <?php
-              if(isset($_SESSION['username'])){
-            ?>
-              <li class="nav-item">
-                <a class="nav-link" href="redacta.php">Redacta</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="images.php">Gestor de mitjos</a>
-              </li>
-
-
-            <?php
-              }
-              if (isset($_SESSION['role']) && $_SESSION['role']=="admin") {
-            ?>
-            <li class="nav-item">
-              <a class="nav-link" href="administra.php">Administra</a>
-            </li>
-            <?php } ?>
-
-          </ul>
-        </div>
-      </div>
-       <div id="userInfo">
-       <?php echo ($userLabel);
-
-       if($user!="Anònim") { ?>
-       <a href="logout.php"> Tanca la sessió</a>
-       <?php } else { ?>
-       <a href="loginForm.php"> Inicia la sessió</a>
-       <?php }?>
-
+        
 
        </div>
             
